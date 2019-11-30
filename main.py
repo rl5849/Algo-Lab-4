d06f50313ade91196d00aaa8546cdab82a97b388
rl5849 <rl5849@rit.edu>
2019-11-30 11:28:42 -0500

    Implemented line finding algo

--- main.py
+++ main.py
@@ -6,8 +6,30 @@ M = 90 ## Max number of chars per line
 
 #If a given line contains words i through j and leave exactly one space between words
 # the number of extra space chars at the eol is: M - j + i - sum(j, k=i)lk
-def DoPrettyPrint(fromFile, toFile):
-    return 0
+def DoPrettyPrint(fromFileName, toFile):
+    with open(fromFileName) as fromFile:
+        content = fromFile.read()
+    newString = ""
+    charOffset=0
+
+    while True:
+        spaces = 0
+        if charOffset + M < len(content):
+            currentLine = content[charOffset : charOffset + M]
+            while currentLine[len(currentLine) - spaces - 1] != " ": #walk back
+                spaces += 1
+            newString += currentLine[0:M-spaces-1] + (" " * spaces) + "\n"
+            charOffset += M-spaces
+        else:
+            currentLine = content[charOffset:]
+            spaces = M - len(currentLine)
+            newString += currentLine + (" " * spaces) + "\n"
+            break
+
+    print(newString)
+
+
+
 
 
 
